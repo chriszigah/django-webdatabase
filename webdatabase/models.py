c4b4d916e77c7282768e919cacd609bc788ec295
Christian Zigah <zigahchristian@yahoo.com>
2024-04-23 14:25:22 +0000

    Create and Register Record Model

--- webdatabase/models.py
+++ webdatabase/models.py
@@ -12,3 +12,5 @@ class Record(models.Model):
     city = models.CharField(max_length=20)
     country = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.first_name + "   " + self.last_name
